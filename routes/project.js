425e3f56b53c0c827692ae5797a63e258a28c127
Mehul Bhatt <mehul.2287884@gmail.com>
2019-03-04 16:13:47 +0530

    for pull

--- routes/project.js
+++ routes/project.js
@@ -4,7 +4,7 @@ var projectController = require('../controller/project.controller');
 var auth = require('./auth');
 
 /* GET home page. */
-router.post('/add-project',, projectController.addProject);
+router.post('/add-project', projectController.addProject);
 router.get('/all', projectController.getAllProject);
 router.get('/get-project-by-id/:projectId', projectController.getProjectById);
 router.delete('/delete/:projectId', projectController.deleteProjectById);
