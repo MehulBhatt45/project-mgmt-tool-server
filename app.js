8d8bab231c0f804f33e54108d9589eebab9c0476
foramtrada232 <foramtrada232@gmail.com>
2019-03-09 16:46:57 +0530

    conflict resolve

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var app = express();
 app.set('superSecret', 'pmt');
 // Define mongoose Component
 
-mongoose.connect('mongodb://localhost:/projectMngtTool', {useNewUrlParser: true})
+mongoose.connect('mongodb://127.0.0.1:27017/projectMngtTool', {useNewUrlParser: true})
 .then(() => console.log("Connected"))
 .catch(err => console.log(err));
 
