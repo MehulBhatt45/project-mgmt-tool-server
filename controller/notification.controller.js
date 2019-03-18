be4322170f072d9b8b0b7989ae98d542f94e6038
vishalpankhaniya <vishal.pankhaniya786@gmail.com>
2019-03-18 18:16:02 +0530

    push notification

--- controller/notification.controller.js
+++ controller/notification.controller.js
@@ -1,7 +1,7 @@
-var notificationModel = require('../model/notice.model');
+var notificationModel = require('../model/notification.model');
 let notificationController = {};
 
-notificationController.addData = function(req,res){
+notificationController.addUser = function(req,res){
 	console.log("notificatiion data",req.body);
 
 	var notification = new notificationModel(req.body);
@@ -10,5 +10,35 @@ notificationController.addData = function(req,res){
 		res.status(200).send(SavedUser);
 	})
 }
+
+notificationController.getAllUsers = function(req, res){
+	notificationModel.find({})
+	.exec((err,users)=>{
+		if (err) {
+			res.status(500).send(err);
+		}else if (users){
+			res.status(200).send(users);
+			req.session.users = users;
+			console.log("response in session",req.session.users);
+		}else{
+			res.status(404).send( { msg : 'Users not found' });
+		}
+	})
+}
+
+notificationController.getUserById = function(req, res){
+	userId = req.params.userId;
+	notificationModel.findOne({userId:userId})
+	.exec((err,users)=>{
+		if (err) {
+			res.status(500).send(err);
+		}else if (users){
+			res.status(200).send(users);
+		}else{
+			res.status(404).send( { msg : 'Users not found' });
+		}
+	})
+}
+
 
 module.exports = notificationController; 
