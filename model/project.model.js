c7405714de5db03ce365d758d45349c9f7024f5a
vivekbharda <vivekkbharda@gmail.com>
2019-04-16 19:41:34 +0530

    for pull

--- model/project.model.js
+++ model/project.model.js
@@ -17,10 +17,10 @@ var ProjectSchema = new Schema({
 		require: true,
 		text: true
 	},
-	clientEmail: {type: String ,required: true},
-	clientFullName:  {type: String,required: true},
-	clientContactNo: {type: String,required: true},
-	clientDesignation: {type: String,required:true},	
+	clientEmail: {type: String },
+	clientFullName:  {type: String},
+	clientContactNo: {type: String},
+	clientDesignation: {type: String},	
 	createdBy: { type: Schema.Types.ObjectId, ref: 'User'},
 	deadline: {type:Date},
 },{timestamps: true});
