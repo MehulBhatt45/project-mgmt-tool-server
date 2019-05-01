825ad3544803bc8abae6517143f06d529858ce0c
vivek bharda <vivekkbharda@gmail.com>
2019-05-01 16:58:07 +0530

    attendence-updated

--- controller/attendence.controller.js
+++ controller/attendence.controller.js
@@ -150,42 +150,46 @@ attendenceController.AllemployeeAttendenceByDate = function(req , res){
 	})
 }
 
-// attendenceController.getAttendenceInInterval = function(req , res){
-// 	req.body.fromDate = moment(req.body.fromDate).format("YYYY-MM-DD");
-// 	fromDate = req.body.fromDate+"T00:00:00.000Z";
-// 	req.body.toDate = moment(req.body.toDate).format("YYYY-MM-DD");
-// 	toDate = req.body.toDate+"T00:00:00.000Z";
-// 	var j;
-// 	var k = 0;
-// 	count = 0;
-// 	var nextDate;
-// 	var allItems = [];
-// 	var items = {
-// 		date: "",
-// 		records: []
-// 	}
+attendenceController.getAttendenceInInterval = function(req , res){
 
-// 	console.log("From Date ========>" , fromDate);
-// 	console.log("to Date ========>" , toDate);
-// 	attendenceModel.find({date : { $gte: fromDate, $lte: toDate }})
-// 	.populate('user_Id')
-// 	.exec(function(err , foundUser){
-// 		console.log("foundUser=================>",foundUser);
+	console.log('body: ', req.body);
+	req.body.fromDate = moment(req.body.fromDate).format("YYYY-MM-DD");
+	var fromDate = req.body.fromDate+"T00:00:00.000Z";
+	req.body.toDate = moment(req.body.toDate).format("YYYY-MM-DD");
+	console.log('todDate: ', req.body.toDate);
+	var toDate = req.body.toDate+"T00:00:00.000Z";
+	console.log('hey tododate: ', toDate);
+
+	var j;
+	var k = 0;
+	count = 0;
+	var nextDate;
+	var allItems = [];
+	console.log('body: ',typeof fromDate, typeof toDate);
+	console.log('body: ',fromDate, toDate);
+
+	attendenceModel.find({date : { $gte: fromDate, $lte: toDate }})
+	.populate('user_Id')
+	.exec(function(err , foundUser){
+		console.log("founduser===============",foundUser);
+		for(i = 0; i<foundUser.length; i++){
+			console.log("allItems==========", foundUser[i]);
+
+			var obj = {
+
+				UserName : foundUser[i].user_Id.name,
+				userId : foundUser[i].user_Id._id,
+				check: foundUser[i].in_out,
+				difference:foundUser[i].difference
+			}
+			// console.log("obj-----------------------",obj);
+			allItems.push(obj);
+
+		}
 		
-// 		console.log("Found User Length ===============>" , foundUser.length);
-// 		items.date = foundUser[0].date;
-// 		allItems[count] = items;
-// 		count++
-// 		var currentDate = foundUser[0].date;
-		
-// 	res.send(allItems);
-// 	for(i = 0; i<allItems.length; i++){
-// 		console.log("hiii");
-// 	}
-		
-// 		res.send(allItems);
-// 	})
-// }
+		res.send(allItems);
+	})
+}
 
 
 
